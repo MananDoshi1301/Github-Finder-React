6294c02053f1cd2d00b61f3954b30770879c5a34
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-22 01:27:32 +0530

    Spinner component created!

---
+++ src/components/layouts/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import spinner from "./spinner.gif";
+
+const Spinner = () => {
+  return (
+    <Fragment>
+      <img src={spinner} alt="Loading..." srcset="" style={{ width: '200px', margin: 'auto', display: 'block' }} />g
+    </Fragment>
+  )
+}
+
+export default Spinner

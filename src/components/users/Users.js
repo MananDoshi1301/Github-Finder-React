9bcc82c30f81b39550f6796721e7398df84834d5
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-22 01:38:28 +0530

    succesfully deployed spinner before fetching

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import UserItem from './UserItem';
+import Spinner from "../layouts/Spinner";
 
-export class Users extends Component {
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />
+  }
+  else {
     return (
       <div style={userStyle}>
-        {this.props.users.map(user => (
+        {users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     )
   }
+}
+
+
+Users.propTypes = {
+  user: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
 }
 
 const userStyle = {

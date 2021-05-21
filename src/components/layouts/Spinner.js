288a84a2664a5fc42c8ed9bc7b3664ce894e63bf
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-22 01:38:11 +0530

    Spinner file created!

--- src/components/layouts/Spinner.js
+++ src/components/layouts/Spinner.js
@@ -3,9 +3,9 @@ import spinner from "./spinner.gif";
 
 const Spinner = () => {
   return (
-    <Fragment>
+    <>
       <img src={spinner} alt="Loading..." srcset="" style={{ width: '200px', margin: 'auto', display: 'block' }} />g
-    </Fragment>
+    </>
   )
 }
 

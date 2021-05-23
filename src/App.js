4bcbfcc17b96b668124261a905706081faab1ffc
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-23 18:55:37 +0530

    searchUsers implemented

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Users from './components/users/Users';
 import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layouts/Alert';
+import GithubState from './context/github/GithubState';
 import './App.css';
 import axios from 'axios';
 import About from './components/pages/About';
@@ -22,14 +23,7 @@ const App = () => {
   //   setState({ users: res.data, loading: false })
   // }
 
-  const searchUsers = async (text) => {
-    setLoading(true);
 
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    setUsers(res.data.items);
-    setLoading(false);
-  }
 
   const getUser = async (username) => {
     setLoading(true);
@@ -63,43 +57,44 @@ const App = () => {
 
 
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={alert} />
-          <Switch>
-            <Route exact path='/'
-              render={props => {
-                return <Fragment>
-                  <Search
-                    searchUsers={searchUsers}
-                    clearUsers={clearUsers}
-                    showClear={users.length > 0 ? true : false}
-                    setAlert={showAlert}
+    <GithubState>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={alert} />
+            <Switch>
+              <Route exact path='/'
+                render={props => {
+                  return <Fragment>
+                    <Search
+                      clearUsers={clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={showAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                }} />
+              <Route
+                exact path={'/about'}
+                component={About} />
+
+              <Route exact path='/user/:login' render={(props) => {
+                return (
+                  <User {...props} getUser={getUser}
+                    getUserRepos={getUserRepos}
+                    user={user}
+                    repos={repos}
+                    loading={loading}
                   />
-                  <Users loading={loading} users={users} />
-                </Fragment>
+                )
               }} />
-            <Route
-              exact path={'/about'}
-              component={About} />
+            </Switch>
 
-            <Route exact path='/user/:login' render={(props) => {
-              return (
-                <User {...props} getUser={getUser}
-                  getUserRepos={getUserRepos}
-                  user={user}
-                  repos={repos}
-                  loading={loading}
-                />
-              )
-            }} />
-          </Switch>
-
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </GithubState>
   );
 
 
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types'
+import React, { useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
-const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+const Search = ({ showClear, clearUsers, setAlert }) => {
+  const githubContext = useContext(GithubContext)
   const [text, setText] = useState('');
 
 
@@ -13,7 +15,7 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
       setAlert('Please enter something', 'light');
     }
     else {
-      searchUsers(text);
+      githubContext.searchUsers(text);
       setText('');
     }
   };
@@ -47,7 +49,6 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
 
 
 Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
   clearUsers: PropTypes.func.isRequired,
   showClear: PropTypes.bool.isRequired,
   setAlert: PropTypes.func.isRequired,

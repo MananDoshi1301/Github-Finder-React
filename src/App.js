3f3776b9b8c133304f9f9927f5bc1b01602f9b2b
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-23 11:39:13 +0530

    Link of username to user page created with routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layouts/Alert';
 import './App.css';
@@ -11,6 +12,7 @@ import About from './components/pages/About';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null,
   }
@@ -26,7 +28,13 @@ class App extends Component {
 
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({ users: res.data.items, loading: false })
+  }
 
+  getUser = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ users: res.data, loading: false })
   }
 
   clearUsers = () => this.setState({ users: [], loading: false });
@@ -38,7 +46,7 @@ class App extends Component {
   }
 
   render() {
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
     return (
       <Router>
         <div className="App">
@@ -61,6 +69,14 @@ class App extends Component {
               <Route
                 exact path={'/about'}
                 component={About} />
+
+              <Route exact path='/user/:login' render={(props) => {
+                return (
+                  <User {...props} getUser={this.getUser} user={user}
+                    loading={loading}
+                  />
+                )
+              }} />
             </Switch>
 
           </div>

78a83781e4dbd3aff60dde7995d5e11485c365f2
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-24 01:20:00 +0530

    Updated with title and about link

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,8 +5,8 @@ const About = () => {
     <Fragment>
       <h1>About This App</h1>
       <p>App to search Github Users</p>
-      <p>Version: 1.0.0</p>
-      <h3>Built By <a href="https://github.com/MananDoshi1301"><u><i>Manan Doshi</i></u></a></h3>
+      <p>Version: 1.0.3</p>
+      <h3>Built By <a href="https://github.com/MananDoshi1301/Github-Finder-React" target="_blank" rel="noreferrer"><u><i>Manan Doshi</i></u></a></h3>
     </Fragment>
   )
 }

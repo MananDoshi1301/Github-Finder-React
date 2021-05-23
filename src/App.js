7ec5077e242a03ac484a40484f025d6ea3685025
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-24 00:26:52 +0530

    Home created to clean App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
-import Users from './components/users/Users';
+import Home from './components/pages/Home';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layouts/Alert';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
@@ -20,19 +19,12 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route exact path='/'
-                  render={props => {
-                    return <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  }} />
+                <Route exact path='/' component={Home} />
                 <Route
                   exact path={'/about'}
                   component={About} />
                 <Route exact path='/user/:login' component={User} />
               </Switch>
-
             </div>
           </div>
         </Router>
